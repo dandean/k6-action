3a157427f06402ca5a3897c8751eeab719860466
Dan Dean <me@dandean.com>
2022-06-01 23:44:11 -0700

    Enable tags?

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 # `$*` expands the `args` supplied in an `array` individually 
 # or splits `args` in a string separated by whitespace.
 export K6_CLOUD_TOKEN=$INPUT_TOKEN
+export K6_STATSD_ENABLE_TAGS=true
 export K6_COMMAND=$([[ $INPUT_CLOUD == "true" ]] && echo 'cloud' || echo 'run')
 sh -c "k6 $K6_COMMAND $INPUT_FILENAME $INPUT_FLAGS"
