86ae35c05c3ef6f169722367979c35602fd543af
Dan Dean <me@dandean.com>
2022-06-01 23:44:50 -0700

    set datadog addr

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@
 # or splits `args` in a string separated by whitespace.
 export K6_CLOUD_TOKEN=$INPUT_TOKEN
 export K6_STATSD_ENABLE_TAGS=true
+export K6_DATADOG_ADDR: datadog-agent:8125
 export K6_COMMAND=$([[ $INPUT_CLOUD == "true" ]] && echo 'cloud' || echo 'run')
 sh -c "k6 $K6_COMMAND $INPUT_FILENAME $INPUT_FLAGS"
